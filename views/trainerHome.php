c5d5fcabd8014a1e350c6306babb596e45a729be
Nayeem <moonnayeem87@gmail.com>
2020-09-21 17:56:09 +0600

    Trainer Home

---
+++ views/trainerHome.php
@@ -0,0 +1,72 @@
+<?php
+
+	require_once('../php/sessionAndCookieHeader.php');
+
+	if(!empty($_SESSION))
+	{
+		if($_SESSION['usertype']!="Trainer")
+		{
+			header('location:../php/logout.php');
+		}
+	}
+	else
+	{
+		if($_COOKIE['usertype']!="Trainer")
+		{
+			header('location:../php/logout.php');
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trainer Home</title>
+</head>
+<body>
+	<fieldset>
+		<p><h1><font color='green'>NSS Training Center</font></h1></p>
+		<?php
+			if(!empty($_SESSION))
+			{
+				echo "<p align='right'><font color='black'>Logged in as </font><a href='viewProfileTrainer.php'><font color='red'>".$_SESSION['userid']."</font></a> | <a href='../php/logout.php'><font color='red'>logout</font></a></p>";
+			}
+			else
+				echo "<p align='right'><font color='black'>Logged in as </font><a href='viewProfileTrainer.php'><font color='red'>".$_COOKIE['userid']."</font></a> | <a href='../php/logout.php'><font color='red'>logout</font></a></p>";
+		?>
+	</fieldset>
+	<fieldset>
+		<table cellspacing="0" cellpadding="5" border="1" width="100%">
+			<tr>
+				<td colspan="10">
+					<ul>
+						<li><a href='trainerHome.php'><font color='red'>Home</font></a></li>
+						<li><a href="viewProfileTrainer.php"><font color="red">View Profile</font></a></li>
+						<li><a href="editProfileTrainer.php"><font color="red">Edit Profile</font></a></li>
+						<li><a href="changePassword.php"><font color="red">Change Password</font></a></li>
+						<li><a href="trainerFile.php"><font color="red">Files</font></a></li>
+						<li><a href="trainerNotice.php"><font color="red">Notices</font></a></li>
+						<li><a href="studentMarks.php"><font color="red">Student Marks</font></a></li>
+						<li><a href="trainerMail.php"><font color="red">Mails</font></a></li>
+						<li><a href="trainerAssignment.php"><font color="red">Assignments</font></a></li>
+						<li><a href="trainerViewClasstime.php"><font color="red">View Class Times</font></a></li>
+						<li><a href="trainerAttendance.php"><font color="red">Upload Attendance</font></a></li>
+						<li><a href="../php/logout.php"><font color="red">Logout</font></a></li>
+					</ul>
+				</td>
+				<td>
+					<?php
+						if(!empty($_SESSION))
+						{
+							echo "<h1 align='center'><font color='black'>Welcome ".$_SESSION['userid']."</font></h1>";
+						}
+						else
+							echo "<h1 align='center'><font color='black'>Welcome ".$_COOKIE['userid']."</font></h1>";
+					?>	
+					<p align="center"><font size="3" color='black'>To NSS Training Center Trainer Home</font></p>
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+</body>
+</html>
